5cd5b757470e366c65efdb1fe7608345e81d06b9
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-03-12 10:25:41 -0300

    first commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Primeiro Projeto React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#121212',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: {
+    color: 'red',
+    fontSize: 24,
+  }
 });
